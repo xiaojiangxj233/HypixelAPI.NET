a3d318b8083e08e86f75530c530f9b5c0e0ca0e0
xiaojiangxj233 <xiaojiang233@outlook.com>
2023-05-10 15:36:10 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace HypixelAPIProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtApiKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvPlayerData = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPlayerName = new System.Windows.Forms.TextBox();
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlayerData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtApiKey
+            // 
+            this.txtApiKey.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtApiKey.Location = new System.Drawing.Point(147, 10);
+            this.txtApiKey.Name = "txtApiKey";
+            this.txtApiKey.Size = new System.Drawing.Size(417, 23);
+            this.txtApiKey.TabIndex = 0;
+            this.txtApiKey.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.label1.Location = new System.Drawing.Point(23, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "API密钥/API key\r\n";
+            // 
+            // dgvPlayerData
+            // 
+            this.dgvPlayerData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPlayerData.Location = new System.Drawing.Point(12, 131);
+            this.dgvPlayerData.Name = "dgvPlayerData";
+            this.dgvPlayerData.RowTemplate.Height = 23;
+            this.dgvPlayerData.Size = new System.Drawing.Size(552, 289);
+            this.dgvPlayerData.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.label2.Location = new System.Drawing.Point(23, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "玩家名/Player Name";
+            // 
+            // txtPlayerName
+            // 
+            this.txtPlayerName.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtPlayerName.Location = new System.Drawing.Point(147, 39);
+            this.txtPlayerName.Name = "txtPlayerName";
+            this.txtPlayerName.Size = new System.Drawing.Size(417, 23);
+            this.txtPlayerName.TabIndex = 3;
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnGetData.Location = new System.Drawing.Point(327, 85);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(237, 40);
+            this.btnGetData.TabIndex = 5;
+            this.btnGetData.Text = "查询/Look up";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.btnGetData_ClickAsync);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.comboBox1.Items.AddRange(new object[] {
+            "all",
+            "4v4",
+            "solo"});
+            this.comboBox1.Location = new System.Drawing.Point(46, 94);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(240, 25);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnGetData);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPlayerName);
+            this.Controls.Add(this.dgvPlayerData);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtApiKey);
+            this.Name = "Form1";
+            this.Text = "Why do you want to play Hypixel?    L";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlayerData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtApiKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvPlayerData;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPlayerName;
+        private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+
+namespace HypixelAPIProgram
+{
+    public partial class Form1 : Form
+    {
+        private const string MOJANG_API_URL = "https://api.mojang.com/users/profiles/minecraft/";
+        private const string HYPIXEL_API_URL = "https://api.hypixel.net/player";
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnGetData_ClickAsync(object sender, EventArgs e)
+        {
+            string apiKey = txtApiKey.Text;
+            string playerName = txtPlayerName.Text;
+            string uuid = await GetUuid(playerName);
+            if (comboBox1.SelectedItem != null)
+            {
+                if (uuid == null)
+                {
+                    MessageBox.Show("Invalid player name.");
+                    return;
+                }
+
+                Dictionary<string, string> playerData = GetBedwarsData(apiKey, playerName, comboBox1.SelectedText);
+                if (playerData == null)
+                {
+                    MessageBox.Show("Failed to get player data.");
+                    return;
+                }
+                // 将玩家数据添加到 DataGridView 中
+                dgvPlayerData.Rows.Clear();
+                foreach (KeyValuePair<string, string> entry in playerData)
+                {
+                    dgvPlayerData.Rows.Add(entry.Key, entry.Value);
+                }
+            }
+            else
+            {
+                MessageBox.Show("你应该选择模式！");
+                return;
+            }
+            
+        }
+        private async Task<string> GetUuid(string playerName)
+        {
+            string url = MOJANG_API_URL + playerName;
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    JsonElement json = JsonSerializer.Deserialize<JsonElement>(responseBody);
+                    return json.GetProperty("id").GetString();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        private Dictionary<string, string> GetBedwarsData(string apiKey, string playerId, string mode)
+        {
+            // 构造 Mojang API 获取 UUID 的 URL
+            var mojangUrl = $"https://api.mojang.com/users/profiles/minecraft/{playerId}";
+            // 发送 GET 请求获取玩家 UUID
+            var client = new HttpClient();
+            var mojangResponse = client.GetAsync(mojangUrl).Result;
+            var mojangContent = mojangResponse.Content.ReadAsStringAsync().Result;
+            var mojangJson = JsonSerializer.Deserialize<JsonElement>(mojangContent);
+            var MUUID = mojangJson.GetProperty("id").GetString();
+
+
+            // 构造 Hypixel API 获取玩家数据的 URL
+            var hypixelUrl = $"https://api.hypixel.net/player?key={apiKey}&uuid={MUUID}&fields=stats.Bedwars";
+
+
+
+            // 发送 GET 请求获取玩家 Bedwars 数据
+            var hypixelResponse = client.GetAsync(hypixelUrl).Result;
+            var hypixelContent = hypixelResponse.Content.ReadAsStringAsync().Result;
+            var hypixelJson = JsonSerializer.Deserialize<JsonElement>(hypixelContent);
+            var bedwarsStats = hypixelJson.GetProperty("player").GetProperty("stats").GetProperty("Bedwars");
+            if (mode == "all")
+            {
+                // 将玩家 Bedwars 数据添加到字典中
+                var bedwarsallData = new Dictionary<string, string>();
+                bedwarsallData.Add("玩家名字", playerId);
+                bedwarsallData.Add("等级", bedwarsStats.GetProperty("level").GetString());
+                bedwarsallData.Add("总游戏次数", bedwarsStats.GetProperty("games_played_bedwars_1").GetString());
+                bedwarsallData.Add("胜利次数", bedwarsStats.GetProperty("wins_bedwars").GetString());
+                bedwarsallData.Add("失败次数", bedwarsStats.GetProperty("losses_bedwars").GetString());
+                bedwarsallData.Add("胜率", bedwarsStats.GetProperty("win_ratio_bedwars").GetString());
+                bedwarsallData.Add("总杀敌数", bedwarsStats.GetProperty("kills_bedwars").GetString());
+                bedwarsallData.Add("总死亡数", bedwarsStats.GetProperty("deaths_bedwars").GetString());
+                bedwarsallData.Add("K/D 比", bedwarsStats.GetProperty("k_d").GetString());
+                bedwarsallData.Add("床摧毁次数", bedwarsStats.GetProperty("beds_broken_bedwars").GetString());
+                bedwarsallData.Add("床丢失次数", bedwarsStats.GetProperty("beds_lost_bedwars").GetString());
+                bedwarsallData.Add("最终击杀数", bedwarsStats.GetProperty("final_kills_bedwars").GetString());
+                bedwarsallData.Add("最终死亡数", bedwarsStats.GetProperty("final_deaths_bedwars").GetString());
+                bedwarsallData.Add("最终 K/D 比", bedwarsStats.GetProperty("final_k_d").GetString());
+                return bedwarsallData;
+            }
+            else if (mode == "4v4")
+            {
+                // 将玩家 bw4v4 数据添加到字典中
+                var bedwars4v4Data = new Dictionary<string, string>();
+                bedwars4v4Data.Add("玩家名字", playerId);
+                bedwars4v4Data.Add("等级", bedwarsStats.GetProperty("level").GetString());
+                bedwars4v4Data.Add("总游戏次数", bedwarsStats.GetProperty("games_played_bedwars_1").GetString());
+                bedwars4v4Data.Add("胜利次数", bedwarsStats.GetProperty("wins_bedwars").GetString());
+                bedwars4v4Data.Add("失败次数", bedwarsStats.GetProperty("losses_bedwars").GetString());
+                bedwars4v4Data.Add("胜率", bedwarsStats.GetProperty("win_ratio_bedwars").GetString());
+                bedwars4v4Data.Add("总杀敌数", bedwarsStats.GetProperty("kills_bedwars").GetString());
+                bedwars4v4Data.Add("总死亡数", bedwarsStats.GetProperty("deaths_bedwars").GetString());
+                bedwars4v4Data.Add("K/D 比", bedwarsStats.GetProperty("k_d").GetString());
+                bedwars4v4Data.Add("床摧毁次数", bedwarsStats.GetProperty("beds_broken_bedwars").GetString());
+                bedwars4v4Data.Add("床丢失次数", bedwarsStats.GetProperty("beds_lost_bedwars").GetString());
+                bedwars4v4Data.Add("最终击杀数", bedwarsStats.GetProperty("final_kills_bedwars").GetString());
+                bedwars4v4Data.Add("最终死亡数", bedwarsStats.GetProperty("final_deaths_bedwars").GetString());
+                bedwars4v4Data.Add("最终 K/D 比", bedwarsStats.GetProperty("final_k_d").GetString());
+            }
+
+
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
